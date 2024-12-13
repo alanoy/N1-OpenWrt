ae7cfc7bc74806e1fc223f2a31ed4e03e777ac15
Alan <alanoy11@gmail.com>
2024-12-13 10:40:49 +0800

    refactor: update Amlogic package and remove Passwall

--- N1/diy.sh
+++ N1/diy.sh
@@ -3,9 +3,11 @@
 # Default IP
 sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 
-# Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
-
 # Add packages
-git clone https://github.com/nantayo/My-Pkg package/my-pkg
-git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+git clone https://github.com/ophub/luci-app-amlogic --depth=1 package/amlogic
+
+# Update packages
+mv package/amlogic/luci-app-amlogic feeds/luci/applications/
+
+# Clean packages
+rm -rf package/amlogic
