02a5cbcef9dcb22928b1fd09b5f3b9db6fb6f451
Alan <alanoy11@gmail.com>
2025-06-16 08:35:03 +0800

    refactor: update N1 workflow for ImmortalWrt 24.10

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Default IP
-sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
-
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
-git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+# Add nikki feed
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
 
 # Update packages
-rm -rf feeds/luci/applications/luci-app-passwall
-cp -fr clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -fr clone/amlogic/luci-app-amlogic feeds/luci/applications/
 
 # Clean packages
 rm -rf clone
