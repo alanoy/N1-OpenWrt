8aad4cefdb1ceb241ed146d2f580d2902decd6ee
Alan <alanoy11@gmail.com>
2025-07-28 15:43:30 +0800

    feature: add luci-app-unblockneteasemusic and nikki feed

--- diy.sh
+++ diy.sh
@@ -2,11 +2,14 @@
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git --depth=1 clone/luci-app-unblockneteasemusic
+
 # Add nikki feed
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
 
 # Update packages
 cp -fr clone/amlogic/luci-app-amlogic feeds/luci/applications/
+cp -fr clone/luci-app-unblockneteasemusic feeds/luci/applications/
 
 # Clean packages
 rm -rf clone
