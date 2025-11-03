1393740f1b5f7f8c811dc40410336d6a44a97380
Alan <alanoy11@gmail.com>
2025-11-03 08:37:26 +0800

    refactor: add GitHub workflow for building ImmortalWrt-24.10 on armsr_armv8

---
+++ armsr/armv8/diy/diy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Add packages
+git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+
+# Add nikki feed
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
+
+# Update packages
+cp -fr clone/amlogic/luci-app-amlogic feeds/luci/applications/
+
+# Clean packages
+rm -rf clone
