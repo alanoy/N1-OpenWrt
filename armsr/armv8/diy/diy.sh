b833a6d6f191af49f766def6b2c9fc8f59e4c285
Alan <alanoy11@gmail.com>
2026-03-16 16:38:33 +0800

    refactor: upgrade to ImmortalWrt 25.12 and cleanup workflows

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -3,14 +3,20 @@
 # Adjust source code
 patch -p1 -f < $(dirname "$0")/luci.patch
 
-# Add packages
+# Clone packages
+git clone https://github.com/nantayo/My-Pkg clone/my-pkg
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+
+git clone https://github.com/Zerogiven-OpenWRT-Packages/luci-app-podman --depth=1 feeds/luci/applications/luci-app-podman
 
 # Add nikki feed
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
 
-# Update packages
+# Adjust packages
 cp -fr clone/amlogic/luci-app-amlogic feeds/luci/applications/
+cp -rf clone/my-pkg/haproxy feeds/packages/net/
+cp -rf clone/my-pkg/podman feeds/packages/utils/
+sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
 # Clean packages
 rm -rf clone
