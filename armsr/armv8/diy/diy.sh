3bf3c87cc10e4ac3c3d0a9e33bd08787f1184e8e
Alan <alanoy11@gmail.com>
2026-06-10 13:40:15 +0800

    refactor: update N1 firmware workflow and configuration for ophub tooling and daed proxy support

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -4,18 +4,16 @@
 patch -p1 -f < $(dirname "$0")/luci.patch
 
 # Clone packages
-git clone https://github.com/nantayo/My-Pkg clone/my-pkg
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
-
+git clone https://github.com/QiuSimons/luci-app-daed clone/daed
 git clone https://github.com/Zerogiven-OpenWRT-Packages/luci-app-podman --depth=1 feeds/luci/applications/luci-app-podman
 
 # Add nikki feed
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
 
 # Adjust packages
-cp -fr clone/amlogic/luci-app-amlogic feeds/luci/applications/
-cp -rf clone/my-pkg/haproxy feeds/packages/net/
-cp -rf clone/my-pkg/podman feeds/packages/utils/
+rm -rf feeds/luci/applications/luci-app-daed feeds/luci/applications/luci-app-passwall
+cp -rf clone/amlogic/luci-app-amlogic clone/daed/luci-app-daed clone/passwall/luci-app-passwall feeds/luci/applications/
 sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
 # Clean packages
